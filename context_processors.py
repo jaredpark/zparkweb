42370677134278bc3da19b0b4e5ea50c41a09719
Jared <jaredjamespark@gmail.com>
2015-03-25 18:56:43 -0700

    seo edits

--- context_processors.py
+++ context_processors.py
@@ -2,13 +2,13 @@ def site_settings_processor(request):
 	context_dictionary = {
 		'root_url': 'www.SurgeSite.com',
 		'admin_name': 'superuser',
-		'company_name': 'SurgeSite Web Design & Development',
+		'company_name': 'SurgeSite Web Design & Web Development', #used in Title tag
 		'logo_file_name': 'images/surgesite_logo.png',
 		'site_email': 'contact@SurgeSite.com',
 		# !!!! important !!!! contact.html has ascii email addresses, must edit those manually
 		'company_phone_text': '',
 		'company_phone_link': '16024299029',
-		'meta_content': 'SurgeSite develops and designs meaningful web based business solutions. We build websites, web applications and execute data-driven pay-per-click campaigns.',
+		'meta_content': 'SurgeSite develops and designs meaningful web based business solutions. We build and maintain websites, web applications and data-driven pay-per-click campaigns.',
 		'navbar_link1_name': '#portfolio',
 		'navbar_link1_text': 'PORTFOLIO',
 		'navbar_link2_name': '#services',
